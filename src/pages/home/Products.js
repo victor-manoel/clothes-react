2847d782d22fd5893b9e555c6567712e914446f1
Victor <victormanoel7895123@hotmail.com>
2021-02-22 22:20:00 -0300

    Install react-loading library and display it to show the products fetching is happening

--- src/pages/home/Products.js
+++ src/pages/home/Products.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
-
+import ReactLoading from "react-loading";
 import axios from "axios";
-
 import Product from "../../components/Product";
 
 const Products = () => {
   // eslint-disable-next-line
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+
     axios
       .get("http://www.mocky.io/v2/59b6a65a0f0000e90471257d")
       .then(({ data }) => setItems(data.products))
-      .catch((response) => console.log(response));
+      .catch((response) => console.log(response))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
@@ -22,9 +25,14 @@ const Products = () => {
       </h1>
       <section className="p-5 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 2xl:px-20">
         {items.map((itemData) => (
-          <Product data={itemData}></Product>
+          <Product key={itemData.id} data={itemData}></Product>
         ))}
       </section>
+      {loading && (
+        <div style={{ position: "absolute", top: "50%", right: "50%" }}>
+          <ReactLoading type="spokes" color="black"></ReactLoading>
+        </div>
+      )}
     </>
   );
 };
