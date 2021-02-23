cf5f13a1e38f9be44411c1f51787df7f28b987f7
Victor <victormanoel7895123@hotmail.com>
2021-02-23 17:27:26 -0300

    Header adjustments

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { BsFillBagFill } from "react-icons/bs";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { GiShoppingCart } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <>
-      <nav className="bg-green-600 py-4 text-white">
+    <header className="bg-green-600 flex justify-between py-4">
+      <nav className=" text-white">
         <ul className="flex flex-wrap justify-center">
-        <li className="mx-2 tracking-widest uppercase font-bold text-black ">
-          <h1>Amaro</h1>
+          <li className="mx-2 tracking-widest uppercase font-bold text-black ">
+            <h1>Amaro</h1>
           </li>
           <li className="mx-2 tracking-widest uppercase font-bold">
-          <NavLink to="/" className="transition-colors hover:text-green-900">
+            <NavLink to="/" className="transition-colors hover:text-green-900">
               Home
             </NavLink>
           </li>
@@ -23,15 +23,17 @@ const Header = () => {
               Promotions
             </NavLink>
           </li>
-            <div className="px-7">
-          <BsFillBagFill title="Add to ShoppingCart"/>
-        </div>
-          
-         
         </ul>
       </nav>
-    </>
-  )
-}
+      <div className="px-7">
+        <GiShoppingCart
+          size={25}
+          color="white"
+          className="inline mb-1"
+        ></GiShoppingCart>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
